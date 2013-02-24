d0ee3fc72d3ca84073bcf348d5c5725e14e13ac7
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2013-02-24 11:30:41 +0100

    WKHTMLToPDF: downgrade to 0.10.0_rc2.
    0.11.0_rc1 has some problems which aren't present in 0.10.0_rc2.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # WKHTMLToPDF Cookbook for Chef
-This will install WKHTMLToPDF 0.11.0 RC1
+This will install WKHTMLToPDF 0.10.0 RC2
--- recipes/default.rb
+++ recipes/default.rb
@@ -24,7 +24,7 @@
 #
 
 execute "install wkhtmltopdf" do
-  command "curl http://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-0.11.0_rc1-static-amd64.tar.bz2 | bunzip2 - | sudo tar xfv -; sudo mv wkhtmltopdf-amd64 wkhtmltopdf"
+  command "curl http://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-0.10.0_rc2-static-amd64.tar.bz2 | bunzip2 - | sudo tar xfv -; sudo mv wkhtmltopdf-amd64 wkhtmltopdf"
   user "root"
   group "root"
   cwd "/usr/local/bin"
